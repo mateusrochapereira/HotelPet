9148ed03cf4f7012951739c342402e9befd2abab
Mateus <mateusrocha.pereira2@gmail.com>
2022-05-10 21:55:45 -0300

    Inicializando projeto

---
+++ src/test/java/com/hotel/pet/HotelPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
